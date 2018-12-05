e52622484ddc7b60ce1b7622210f27124650b040
ebrown <ebrown7871@gmail.com>
2018-12-05 10:00:19 -0800

    Added status handler

--- src/main/java/protocol/protomessage/server/ServerMessageChannel.java
+++ src/main/java/protocol/protomessage/server/ServerMessageChannel.java
@@ -12,6 +12,7 @@ import protobuf.ProtoMessages.ProtoMessage;
 import protocol.protomessage.ExceptionHandler;
 import protocol.protomessage.MessageHandler;
 import protocol.protomessage.MessageTransceiver;
+import protocol.protomessage.StatusHandler;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -35,6 +36,7 @@ public class ServerMessageChannel extends ChannelInitializer<SocketChannel> {
     p.addLast("frameEncoder", new ProtobufVarint32LengthFieldPrepender());
     p.addLast("protobufEncoder", new ProtobufEncoder());
     p.addLast(new MessageHandler(channelIds.incrementAndGet(),transceiver));
+    p.addLast(new StatusHandler());
     p.addLast("idleStateHandler", new IdleStateHandler(0, WRITE_IDLE_TIME, 0));
     p.addLast("heartBeatHandler", new ServerHeartbeatHandler());
     p.addLast(new ExceptionHandler());
